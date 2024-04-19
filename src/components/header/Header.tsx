726d2ff8178c544aca2ef2ef3e09922727f3e706
SamuelT19 <samitesfay15@gmail.com>
2024-04-19 10:44:20 +0300

    .....

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className="header_container">
       <h1>
-        React <span style={{ color: "red" }}>Menu Extraction</span> & 
-        <span style={{ color: "#6161cf" }}>  Visualization</span>
-        <small style={{ fontSize: "13px", color: "black" }}>  @Samuel_T</small>
+        React <span style={{ color: "red" }}>Menu Extraction</span> &
+        <span style={{ color: "#6161cf" }}> Visualization</span>
       </h1>
+      <h4 className="mine">@Samuel_T</h4>
     </div>
   );
 };
